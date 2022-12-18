d11c0551820e8a023d0b657a7eca7337180fd658
jayeshbhavsar03 <bhavsarjayesh9370@gmail.com>
2022-12-18 22:59:22 +0530

    first commit

---
+++ assets/js/common.js
@@ -0,0 +1,11 @@
+function convertToJSON(response){
+    return response.json();
+}
+
+document.getElementById('nav-search-button').addEventListener('click', function(){
+    window.location.href = "/assets/search.html";
+})
+
+document.getElementById('main-title').addEventListener('click', function(){
+    window.location.href = "/assets/";
+})
